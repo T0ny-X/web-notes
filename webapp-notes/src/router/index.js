c61650b242282bf4b5cdcb44b700784124048e6c
Tony <qtx2023@bu.edu>
2023-12-10 17:07:33 -0500

    Fix cap

--- webapp-notes/src/router/index.js
+++ webapp-notes/src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/js',
       name: 'JS',
-      component: () => import('../views/JSView.vue')
+      component: () => import('../views/JsView.vue')
     },
     {
       path: '/html',
